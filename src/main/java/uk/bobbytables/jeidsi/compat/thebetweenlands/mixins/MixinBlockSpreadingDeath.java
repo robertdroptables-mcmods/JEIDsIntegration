80650848fc9bf5b1a070a5de91cdfcecb438e7e7
Sam Kirby <sam.kirby94@hotmail.co.uk>
2019-04-04 01:13:30 +0100

    Small fixes, remove unused imports, style

--- src/main/java/uk/bobbytables/jeidsi/compat/thebetweenlands/mixins/MixinBlockSpreadingDeath.java
+++ src/main/java/uk/bobbytables/jeidsi/compat/thebetweenlands/mixins/MixinBlockSpreadingDeath.java
@@ -6,7 +6,6 @@ import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.Chunk;
 import org.dimdev.jeid.INewChunk;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -14,7 +13,7 @@ import thebetweenlands.common.block.terrain.BlockSpreadingDeath;
 
 @Mixin(BlockSpreadingDeath.class)
 public class MixinBlockSpreadingDeath {
-    @Inject(method = "convertBiome", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "convertBiome", at = @At("HEAD"), cancellable = true, remap = false)
     private void convertBiomes(World world, BlockPos pos, Biome biome, CallbackInfo ci) {
         Chunk chunk = world.getChunk(pos);
         ((INewChunk) chunk).getIntBiomeArray()[(pos.getZ() & 15) << 4 | pos.getX() & 15] = Biome.getIdForBiome(biome);

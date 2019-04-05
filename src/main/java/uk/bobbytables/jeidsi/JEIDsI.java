86a569cc9f7ce0897fee6e8155154def1f57c2c4
Sam Kirby <sam.kirby94@hotmail.co.uk>
2019-04-05 02:26:32 +0100

    CyclopsCore support
    Replace WorldHelpers#setBiome in CyclopsCore so that EvilCraft and other dependencies can update the biome as intended

--- src/main/java/uk/bobbytables/jeidsi/JEIDsI.java
+++ src/main/java/uk/bobbytables/jeidsi/JEIDsI.java
@@ -10,7 +10,7 @@ import uk.bobbytables.jeidsi.network.JEIDsIPacketHandler;
 public class JEIDsI {
     public static final String MODID = "jeidsi";
     public static final String NAME = "JustEnoughIDs Integration";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "@VERSION@";
     
     public static final Logger LOGGER = LogManager.getLogger("JEIDsI");
 
---
+++ src/main/java/uk/bobbytables/jeidsi/compat/cyclopscore/mixins/MixinWorldHelpers.java
@@ -0,0 +1,29 @@
+package uk.bobbytables.jeidsi.compat.cyclopscore.mixins;
+
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import org.cyclops.cyclopscore.helper.WorldHelpers;
+import org.dimdev.jeid.INewChunk;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import uk.bobbytables.jeidsi.network.JEIDsIPacketHandler;
+import uk.bobbytables.jeidsi.network.client.BiomeChangeMessage;
+
+@Mixin(WorldHelpers.class)
+public class MixinWorldHelpers {
+    @Overwrite
+    public static void setBiome(World world, BlockPos pos, Biome biome) {
+        Chunk chunk = world.getChunk(pos);
+        ((INewChunk) chunk).getIntBiomeArray()[(pos.getZ() & 0xF) << 4 | pos.getX() & 0xF] = Biome.getIdForBiome(biome);
+        chunk.markDirty();
+        if (!world.isRemote) {
+            JEIDsIPacketHandler.INSTANCE.sendToAllAround(
+                    new BiomeChangeMessage(pos.getX(), pos.getZ(), Biome.getIdForBiome(biome)),
+                    new NetworkRegistry.TargetPoint(world.provider.getDimension(), pos.getX(), 128.0D, pos.getZ(), 128.0D)
+            );
+        }
+    }
+}
